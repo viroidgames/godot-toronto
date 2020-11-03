ca2a5bc61ecac40aa64288f15f0c0c36c72cfded
Kumsal Obuz <info@viroidgames.com>
2020-11-03 14:09:14 -0500

    first commit

---
+++ Presentations/#3/Scripts/Data.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var selectors:=[
+	{title="STANDARD",price=20,features="as"},
+	{title="BASIC",price=30,features="as"},
+	{title="PROFESSIONAL",price=50,features="as",highlighted=true},
+	{title="ULTRA",price=99,features="as"}
+	]
